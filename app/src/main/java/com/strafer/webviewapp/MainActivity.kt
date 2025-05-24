6bb3d3850d0a4ace4c6b4ee6555457eed8d62667
Strafer1337 <vova.ilin2000@yandex.ru>
2025-05-24 21:42:36 +0300

    Initial commit

---
+++ app/src/main/java/com/strafer/webviewapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.strafer.webviewapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.kevinnzou.web.LoadingState
+import com.kevinnzou.web.WebView
+import com.kevinnzou.web.rememberWebViewState
+import com.strafer.webviewapp.ui.theme.WebViewAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // enableEdgeToEdge()
+        setContent {
+            WebViewAppTheme {
+                Column {
+                    val testLink = "https://prokatis.ru"
+                    var textFieldValue by rememberSaveable { mutableStateOf(testLink) }
+                    TextField(
+                        value = textFieldValue,
+                        onValueChange = { newValue -> textFieldValue = newValue },
+                    )
+                    val state = rememberWebViewState(testLink)
+                    Text(text = "${state.pageTitle}")
+                    val loadingState = state.loadingState
+                    if (loadingState is LoadingState.Loading) {
+                        LinearProgressIndicator(
+                            progress = loadingState.progress,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                    WebView(
+                        state
+                    )
+                }
+            }
+        }
+    }
+}
